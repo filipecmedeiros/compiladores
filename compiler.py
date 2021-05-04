0fc87d72e2ca4caac9f9f674c76e26484c525f10
Filipe Medeiros <filipecmedeiros88@gmail.com>
2021-05-04 19:15:01 -0300

    Add final message

--- compiler.py
+++ compiler.py
@@ -4,6 +4,7 @@ from src.Parser import Parser
 def main():
     parser = Parser(sys.argv[1])
     parser.run()
+    print ("Programa compilado com êxito")
             
 if __name__ == "__main__":
     main()
