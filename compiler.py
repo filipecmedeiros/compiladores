16488766425b5966b96d8ea256f81733a8273092
Filipe <filipecmedeiros88@gmail.com>
2021-06-14 03:17:54 -0300

    Intermediary code generator - variable declaration

--- compiler.py
+++ compiler.py
@@ -4,6 +4,7 @@ from src.Parser import Parser
 def main():
     parser = Parser(sys.argv[1])
     parser.run()
+    parser.close_code()
     print ("Programa compilado com êxito")
             
 if __name__ == "__main__":
